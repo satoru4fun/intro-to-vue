dd834dd61eb3f64e6af0a6ff69f264eb48531fcf
Satoru Noguchi <satoru.nog@gmail.com>
2019-07-14 15:31:27 -0300

    5. Event Handling

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ var app = new Vue({
         description: 'A pair of warm, fuzzy socks',
         image: './vmSocks-green-onWhite.jpg',
         altText: 'A pair of socks',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: 'true',
-        onSale: 'true'
+        onSale: 'true',
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct: function(variantImage) {
+            this.image = variantImage
+        }
     }
 })
