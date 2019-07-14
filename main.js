65d403cb1ba1ad1ad000763aba179a1357467484
Satoru Noguchi <satoru.nog@gmail.com>
2019-07-13 22:49:09 -0300

    3. Conditional Rendering

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: './vmSocks-green-onWhite.jpg',
+        altText: 'A pair of socks',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inStock: 'true',
+        onSale: 'true'
+    }
+})
